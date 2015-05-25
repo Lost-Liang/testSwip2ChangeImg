05e3ecf9f261f4a2d9ac814f3a8daa9eaa2b6a5b
Lost-Liang <liangxiechao@liangxiechaodeMacBook-Pro.local>
2015-05-25 10:36:45 +0800

    Initial Commit

---
+++ testSwip2ChangeImg/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  testSwip2ChangeImg
+//
+//  Created by liangxiechao on 15/5/25.
+//  Copyright (c) 2015年 liangxiechao. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ testSwip2ChangeImgTests/testSwip2ChangeImgTests.m
@@ -0,0 +1,40 @@
+//
+//  testSwip2ChangeImgTests.m
+//  testSwip2ChangeImgTests
+//
+//  Created by liangxiechao on 15/5/25.
+//  Copyright (c) 2015年 liangxiechao. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <XCTest/XCTest.h>
+
+@interface testSwip2ChangeImgTests : XCTestCase
+
+@end
+
+@implementation testSwip2ChangeImgTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    XCTAssert(YES, @"Pass");
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
